ebd56ecff3fc3ffabcd5bbca4ec4b0dcb96872b0
ahmed elmansy <ahmedelmansy0022@gmail.com>
2021-10-01 18:13:23 +0200

    m

---
+++ Business Objects/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Business_Objects.Interfaces
+{
+    interface IUnitOfWork<T> where T : class
+    {
+        IGenericRepository<T> Entity { get; }
+        void Save();
+    }
+}
+
---
+++ Business Objects/Interfaces/Services/IWorkService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+namespace Business_Objects.Interfaces.Services
+{
+    interface IWorkService<T> where T : class
+    {
+        
+        ///
+        /// Get all items of Work table
+        /// 
+        /// 
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+    }
+   
+}
